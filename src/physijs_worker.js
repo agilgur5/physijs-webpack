dd926c5040ce698d21e692ac50e79e5e635a44e3
Kevin Gilbert <kevin@kegisiteweb.com>
2018-03-16 09:24:15 -0400

    fixed collision detection with newer version of ammo

--- src/physijs_worker.js
+++ src/physijs_worker.js
@@ -1252,8 +1252,19 @@ reportCollisions = function() {
 			pt = manifold.getContactPoint( j );
 			//if ( pt.getDistance() < 0 ) {
 				offset = 2 + (collisionreport[1]++) * COLLISIONREPORT_ITEMSIZE;
-				collisionreport[ offset ] = _objects_ammo[ manifold.getBody0() ];
-				collisionreport[ offset + 1 ] = _objects_ammo[ manifold.getBody1() ];
+
+				var body0 = manifold.getBody0();
+				if(typeof body0.ptr !== 'undefined'){
+                    body0 = body0.ptr;
+				}
+
+				var body1 = manifold.getBody1();
+				if (typeof body1.ptr !== 'undefined') {
+                    body1 = body1.ptr;
+				}
+
+				collisionreport[ offset ] = _objects_ammo[ body0 ];
+				collisionreport[ offset + 1 ] = _objects_ammo[ body1 ];
 
 				_vector = pt.get_m_normalWorldOnB();
 				collisionreport[ offset + 2 ] = _vector.x();
