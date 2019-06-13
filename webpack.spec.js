1245a209cd588c5ad9481bbf3d4580f555ce69d4
Anton Gilgur <agilgur5@gmail.com>
2019-06-13 19:46:54 -0400

    (test): add a similar webpack test as well
    - setup import, export, and spec virtually the same as browserify
    - run webpack in dev mode to speed up the build significantly
      - dev mode matches browserify's default too
      - might be able to disable some other 0CJS transforms but speed is
        fine for now
    
    (deps): add webpack, webpack-cli, and worker-loader

---
+++ webpack.spec.js
@@ -0,0 +1,8 @@
+import test from 'ava'
+
+import Physijs from './test-utils/webpack/_import-bundle-to-module.js'
+
+test('Scene creation', t => {
+  const scene = new Physijs.Scene()
+  t.is(scene.type, 'Scene')
+})
