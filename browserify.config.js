8021e1f15cd021552088ac634626ac66ac4805ac
Anton Gilgur <agilgur5@gmail.com>
2019-06-17 19:27:03 -0400

    (optim): don't parse ammo, keep three external
    - speeds up builds
    - tests still pass, so I think it's okay to do, but this may be
      different from how developers use it so not necessarily ideal
      test-prod parity
      - though it may make sense to advocate for noParse as
        performance optimizations
    
    - getting three to be properly require'd as an external with webpack
      meant I had to use libraryTarget for some reason...
      - otherwise it would expect it as a global

--- browserify.config.js
+++ browserify.config.js
@@ -3,5 +3,9 @@ const fs = require('fs')
 
 browserify({
   entries: './test-utils/browserify/_export-to-window.js',
-}).bundle()
+  // don't parse ammo
+  noParse: ['vendor/ammo.js'],
+  // don't bundle three
+}).external('three')
+  .bundle()
   .pipe(fs.createWriteStream('./build/browserify.bundle.js'))
