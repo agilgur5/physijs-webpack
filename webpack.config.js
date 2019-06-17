ea682a5ded0d15fe3b7ba4f1e87325f21feba367
Anton Gilgur <agilgur5@gmail.com>
2019-06-17 19:27:03 -0400

    (optim): don't parse ammo, keep three external
    - speeds up builds
    - tests still pass, so I think it's okay to do, but this may be
      different from how developers use it so not necessarily ideal
      test-prod parity
      - though it may make sense to advocate for noParse as
        performance optimizations
    
    - getting three to be properly require'd as an external with webpack
      meant I had to use libraryTarget for some reason...
      - otherwise it would expect it as a global

--- webpack.config.js
+++ webpack.config.js
@@ -4,8 +4,16 @@ module.exports = {
   entry: './test-utils/webpack/_export-to-window.js',
   output: {
     path: path.join(__dirname, '/build/'),
-    filename: 'webpack.bundle.js'
+    filename: 'webpack.bundle.js',
+    library: 'Physijs',
+    libraryTarget: 'umd'
   },
   // fast builds, build is only used for testing purposes
-  mode: 'development'
+  mode: 'development',
+  // don't bundle three
+  externals: /^three$/,
+  // don't parse ammo
+  module: {
+    noParse: path.join(__dirname, '/vendor/ammo.js')
+  }
 }
