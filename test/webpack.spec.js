f1378c0f5e51c1e4dd8a4812b045dbca5b20dd16
Anton Gilgur <agilgur5@gmail.com>
2019-11-22 15:40:14 -0500

    (refactor): move test files into test/
    - app root has a lot of files in it, this should help declutter a bit

--- test/webpack.spec.js
+++ test/webpack.spec.js
@@ -1,6 +1,6 @@
 import test from 'ava'
 
-import Physijs from './test/utils/webpack/_import-bundle-to-module.js'
+import Physijs from './utils/webpack/_import-bundle-to-module.js'
 
 test('Scene creation', t => {
   const scene = new Physijs.Scene()
